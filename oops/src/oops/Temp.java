d8f20d40140b483a2214316db1c6f28fa75ae076
nitin <nitin@nitin-PC>
2018-10-10 12:43:02 +0530

    Cheking oops complete pages

---
+++ oops/src/oops/Temp.java
@@ -0,0 +1,20 @@
+package oops;
+
+public class Temp
+{
+  int x =10;
+  Temp()
+  {
+	  System.out.println(this);
+  }
+public static void main(String...z)
+{
+	Temp  t = new Temp();
+	System.out.println(t);
+	
+	
+	
+}
+
+}
+
